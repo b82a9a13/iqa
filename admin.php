a3430277d6726fac804ad06b2e1206321e8b45c7
b82a9a13 <85174237+b82a9a13@users.noreply.github.com>
2023-09-04 12:15:18 +0100

    v0.1.0

---
+++ admin.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package     local_iqa
+ * @author      Robert Tyrone Cullen
+ * @var stdClass $plugin
+ */
+
+require_once(__DIR__.'/../../config.php');
+require_login();
+$context = context_system::instance();
+require_capability('local/iqa:admin', $context);
+use local_iqa\lib;
+$lib = new lib;
+$p = 'local_iqa';
+$title = get_string('iqa_a', $p);
+
+$PAGE->set_context($context);
+$PAGE->set_url(new moodle_url('/local/iqa/admin.php'));
+$PAGE->set_title($title);
+$PAGE->set_heading($title);
+$PAGE->set_pagelayout('admin');
+
+echo $OUTPUT->header();
+
+$template = (Object)[
+    'title' => $title
+];
+echo $OUTPUT->render_from_template('local_iqa/admin', $template);
+
+echo $OUTPUT->footer();
+$_SESSION['iqa_admin'] = true;
---
+++ lang/en/local_iqa.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Strings for component 'local_iqa', language 'en'
+ * @package   local_iqa
+ * @author      Robert Tyrone Cullen
+ */
+
+//A
+//B
+//C
+//D
+//E
+//F
+//G
+//H
+//I
+    $string['iqa_a'] = 'IQA Administration';
+//J
+//L
+//M
+//N
+//O
+//P
+    $string['pluginname'] = 'IQA';
+//Q
+//R
+//S
+//T
+//U
+//V
+//W
+//X
+//Y
+//Z
